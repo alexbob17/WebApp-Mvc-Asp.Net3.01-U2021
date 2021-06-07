0d13a1a1c5d43c60f5653b40a0978089379aec16
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-06-07 14:12:56 -0600

    add contralador edit and delete

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -9,7 +9,8 @@ namespace turnos.Controllers
     public class EspecialidadController : Controller
     {
         private readonly TurnosContext _context;
-        public EspecialidadController(TurnosContext context){
+        public EspecialidadController(TurnosContext context)
+        {
 
             _context = context;
         }
@@ -18,6 +19,49 @@ namespace turnos.Controllers
         {
             // Devuelve la tabla de especialidad
             return View(_context.Especialidad.ToList());
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var especialidad = _context.Especialidad.Find(id);
+
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        [HttpPost] //Este envia el edit
+        public IActionResult Edit(int id, [Bind("IdEspecialidad, Descripcion")] Especialidad especialidad)
+        {
+            if (id != especialidad.IdEspecialidad)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(especialidad);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var especialidad = _context.Especialidad.FirstOrDefault(e => e.IdEspecialidad == id);
+            
+            return View(especialidad);
         }
     }
 }
