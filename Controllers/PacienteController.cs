62fb1ffa37e4a853a396af9d5d3f8ff814ef585e
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-06-07 18:04:22 -0600

    Adding paciente controller(Create)

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -9,13 +9,51 @@ namespace turnos.Controllers
     {
         private readonly TurnosContext _context;
 
-        public PacienteController(TurnosContext context){
+        public PacienteController(TurnosContext context)
+        {
             _context = context;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.Paciente.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.IdPaciente == id);
+
+            if (Paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(Paciente);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Modelo crear Paciente
+        [HttpPost]
+        //Valida que el formulario no se acceda atraves de una url , Sino desde el formulario
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPaciente,Nombre,Apellido,Direccion,Telefono,Email")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
         }
     }
 }
