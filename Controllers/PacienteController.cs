d9e324bac080bf599e1589165145a4a7b5008fd6
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-06-08 15:26:48 -0600

    añadiendo en el modelo vista (Controlador Delete)

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -53,7 +53,79 @@ namespace turnos.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View();
+            return View(paciente);
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var paciente = await _context.Paciente.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            return View(paciente);
+        }
+
+        //Modelo crear Paciente
+        [HttpPost]
+        //Valida que el formulario no se acceda atraves de una url , Sino desde el formulario
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPaciente,Nombre,Apellido,Direccion,Telefono,Email")] Paciente paciente)
+        {
+            if (id != paciente.IdPaciente)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(paciente);
+                await _context.SaveChangesAsync();
+                RedirectToAction(nameof(Index));
+            }
+            return View(paciente);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.IdPaciente == id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            return View(paciente);
+        }
+
+        //Modelo crear Paciente
+        [HttpPost, ActionName("Delete")]
+        //Valida que el formulario no se acceda atraves de una url , Sino desde el formulario
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var paciente = await _context.Paciente.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Paciente.Remove(paciente);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
