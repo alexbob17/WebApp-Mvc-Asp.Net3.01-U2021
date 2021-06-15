23c33309a144323e0d9f19b0ae409db5be29fa66
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-06-15 00:01:08 -0600

    changes the styles and models(View)

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -6,6 +6,8 @@ namespace turnos.Models
     {
         [Key]
         public int  IdEspecialidad{ get; set; }
+        [Required (ErrorMessage ="Debe ingresar una descripción")]
+        [Display(Name ="Descripción", Prompt ="Ingrese una descripción")]
         public string Descripcion {get; set;}
         public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
 
