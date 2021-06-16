bd1647aaa308b29e09ff7cd9464264fbe8261fad
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-06-15 21:36:24 -0600

    Controlador Turnos(Full Calendar.View)

--- Controllers/TurnoController.cs
+++ Controllers/TurnoController.cs
@@ -15,15 +15,16 @@ namespace turnos.Controllers
 
         private IConfiguration _configuration;
 
-        private TurnoController(TurnosContext context, IConfiguration configuration)
+        public TurnoController(TurnosContext context, IConfiguration configuration)
         {
             _context = context;
             _configuration = configuration;
         }
 
-        public IActionResult Index(){
-            ViewData["IdMedico"] = new SelectList((from medico in _context.Medico.ToList() select new{ idMedico = medico.IdMedico, NombreCompleto = medico.Nombre + " " + medico.Apellido}),"IdMedico", "NombreCompleto");
-            ViewData["IdPaciente"] = new SelectList((from paciente in _context.Paciente.ToList() select new{ idPaciente = paciente.IdPaciente, NombreCompleto = paciente.Nombre + " " + paciente.Apellido}),"IdPaciente", "NombreCompleto");
+        public IActionResult Index()
+        {
+            ViewData["IdMedico"] = new SelectList((from medico in _context.Medico.ToList() select new{ IdMedico = medico.IdMedico, NombreCompleto = medico.Nombre + " " + medico.Apellido}),"IdMedico", "NombreCompleto");
+            ViewData["IdPaciente"] = new SelectList((from paciente in _context.Paciente.ToList() select new{ IdPaciente = paciente.IdPaciente, NombreCompleto = paciente.Nombre + " " + paciente.Apellido}),"IdPaciente", "NombreCompleto");
             return View();
         }
 
